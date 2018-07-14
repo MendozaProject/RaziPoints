61087e2e384dd4eb351e02d7e31e2ac273e1131a
Angelo Mendoza <themendozaproject@gmail.com>
2018-07-13 21:49:32 -0700

    Added separate packages

--- app/src/main/java/com/computersquid/razipoints/ui/activity/TheWillOfGod.kt
+++ app/src/main/java/com/computersquid/razipoints/ui/activity/TheWillOfGod.kt
@@ -1,7 +1,8 @@
-package com.computersquid.razipoints
+package com.computersquid.razipoints.ui.activity
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import com.computersquid.razipoints.R
 
 class TheWillOfGod : AppCompatActivity() {
 
